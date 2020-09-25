dd2446c5b6ba9614e1b552911324d4a5e60377f0
Ebin Paul <ebinpaul@outlook.com>
2020-09-25 11:27:46 +0530

    Update models.py

--- social_clone_project/posts/models.py
+++ social_clone_project/posts/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        reverse('posts:single', kwargs={'username': self.user.username, 'pk':self.pk})
+        return reverse('posts:single', kwargs={'username': self.user.username, 'pk':self.pk})
 
     class Meta:
         ordering = ['-created_at']
