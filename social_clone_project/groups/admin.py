1d1f8e3ab1aacd83ded32bd9fb996851332ada39
Ebin Paul <ebinpaul@outlook.com>
2020-09-25 13:47:49 +0530

    Update admin.py

--- social_clone_project/groups/admin.py
+++ social_clone_project/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
+
+# This class enables for a dbadmin to click on row containing the group details and edit group members from the same.
+class GroupMemberInLine(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
