4436ad63409d531e5d3210d3aaf0f1b200059de2
Ebin Paul <ebinpaul@outlook.com>
2020-09-25 13:34:40 +0530

    Groups update

--- social_clone_project/groups/views.py
+++ social_clone_project/groups/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Group, GroupMember
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name','description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroup(generic.ListView):
+    model = Group
