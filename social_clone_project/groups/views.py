850991da5867c482ad34cf0ef8e2af130d77c656
Ebin Paul <ebinpaul@outlook.com>
2020-09-27 13:37:13 +0530

    Update views.py

--- social_clone_project/groups/views.py
+++ social_clone_project/groups/views.py
@@ -1,8 +1,11 @@
+from django.db.utils import IntegrityError
 from django.shortcuts import render
 from .models import Group, GroupMember
 from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
 from django.urls import reverse
 from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
@@ -14,3 +17,22 @@ class SingleGroup(generic.DetailView):
 
 class ListGroup(generic.ListView):
     model = Group
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+    
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except IntegrityError:
+            messages.warning(self.request,'Warning already a member!')
+        else:
+            messages.success(self.request,'You are now a member!')
+
+        return super().get(request, *args, **kwargs)
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+    pass
